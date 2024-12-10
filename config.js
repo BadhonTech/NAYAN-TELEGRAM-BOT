a5ce4a790cbca4a20ebfeac70ab3123a58aaa525
BadhonTech <163644806+BadhonTech@users.noreply.github.com>
2024-12-10 22:43:40 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
-  ownerNumber: "+8801615298449",//Your Number
-  admin: ["5920596290"], // your id
+  ownerUsernames: ["BADHON"],//Your Username
+  admin: ["6529583604"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "Your_Bot_Token",//Your Bot Tokens
+  telegramBotToken: "7890456767:AAEaCcg80eLbskZWXJnYU7KrXZ7XJlFaLdI",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/6529583604",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
